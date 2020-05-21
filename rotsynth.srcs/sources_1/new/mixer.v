d6346959664a0df45744b6163c9fa883bd297b68
Callum Tilbury <TLBCAL002@myuct.ac.za>
2020-05-21 19:53:00 +0200

    Added module heading and basic structure.

---
+++ rotsynth.srcs/sources_1/new/mixer.v
@@ -0,0 +1,21 @@
+/*
+    Mixer module
+    EEE4120F: YODA Project
+
+    Written by:     Callum Tilbury
+    Last updated:   21-May-2020
+
+    ---
+
+    Inputs:
+    Outputs:
+
+    ---
+
+*/
+
+module Mixer();
+
+
+
+endmodule
