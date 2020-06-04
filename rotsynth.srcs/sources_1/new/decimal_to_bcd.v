e4eea4e5bdb7bce8a140dafa4b3e4ab33a096170
SpectrumX <wylieza@gmail.com>
2020-06-04 11:23:37 +0200

    created decimal_to_bcd module - contains the module description format style

---
+++ rotsynth.srcs/sources_1/new/decimal_to_bcd.v
@@ -0,0 +1,21 @@
+/**************************************************************************************
+------ Module Description -----
+Author > Justin Wylie
+
+Description > Convert an integer value to bcd format
+
+Usage:
+Accepts > 14 bit number with value < 9999
+Returns > Four 4 bit numbers corresponding to the 4 bcd digits of the inputted number
+
+Clocking & Reset:
+Requires Clock > No
+Reset Bit > No
+Enable Bit > No
+
+**************************************************************************************/
+
+module decimal_to_bcd(
+
+    );
+endmodule
