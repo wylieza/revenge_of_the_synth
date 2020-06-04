0692dd4ff88bdedf71ed00c9cf04954c767b36b7
SpectrumX <wylieza@gmail.com>
2020-06-04 10:32:20 +0200

    using keegans provided code for bcd decoding & updated constraints file for ssd enabling

---
+++ rotsynth.srcs/sources_1/new/bcd_decoder.v
@@ -0,0 +1,28 @@
+//Code Origin Notice//
+//This code was provided by the EEE4020 course and has been reused here in this project
+
+module bcd_decoder(
+    input [3:0]BCD,
+    output reg [6:0]SevenSegment
+);
+//------------------------------------------------------------------------------
+// Combinational circuit to convert BCD input to seven-segment output
+always @(BCD) 
+begin
+case(BCD)
+    // gfedcba
+    4'd0 : SevenSegment <= 7'b0111111;  //     a
+    4'd1 : SevenSegment <= 7'b0000110;  //    ----
+    4'd2 : SevenSegment <= 7'b1011011;  //   |   |
+    4'd3 : SevenSegment <= 7'b1001111;  //  f| g |b
+    4'd4 : SevenSegment <= 7'b1100110;  //    ----
+    4'd5 : SevenSegment <= 7'b1101101;  //   |   |
+    4'd6 : SevenSegment <= 7'b1111101;  //  e|   |c
+    4'd7 : SevenSegment <= 7'b0000111;  //    ----
+    4'd8 : SevenSegment <= 7'b1111111;  //      d
+    4'd9 : SevenSegment <= 7'b1101111;
+default: SevenSegment <= 7'b0000000;
+endcase
+end
+//--------------------------------------------------------------
+endmodule 
