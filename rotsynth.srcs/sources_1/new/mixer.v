5b3e691306ed6bfb9086c88cac4565c5b5ae926c
Callum Tilbury <TLBCAL002@myuct.ac.za>
2020-06-04 16:49:41 +0200

    Updated comments to match standard format.

--- rotsynth.srcs/sources_1/new/mixer.v
+++ rotsynth.srcs/sources_1/new/mixer.v
@@ -1,21 +1,27 @@
-/*
-    Mixer module
-    EEE4120F: YODA Project
+/**************************************************************************************
+------ Module Description -----
+Name            > Mixer.v
+Author          > Callum Tilbury
 
-    Written by:     Callum Tilbury
-    Last updated:   21-May-2020
+Description     > Uniformly mix a bus of 8 input signals into a single output
 
-    ---
+Usage:
+ Accepts        > 8 x 11-bit signals
+                > 1 x  8-bit enable bitmask
+ Returns        > 1 x 11-bit mixed signal
 
-    Inputs:     8 x 11-bit signals
-    Outputs:    1 x 11-bit signal
-
-    ---
-
-*/
+Clocking & Reset:
+ Requires Clock > No
+ Reset Bit      > No
+ Enable Bit     > No ... (enable bitmask — see above)
+**************************************************************************************/
 
 module mixer(
-    // Inputs
+    // Signal Inputs
+    // TODO: Would it be better to do this:
+    //          input [10:0] signals [0:7]  ?
+    //  Then, when inputting to the module, you can just concatenate 
+    //     the input signals: mixer({signal_a, signal_b, ...})?
     input [10:0] signal_a,
     input [10:0] signal_b,
     input [10:0] signal_c,
@@ -24,11 +30,13 @@ module mixer(
     input [10:0] signal_f,
     input [10:0] signal_g,
     input [10:0] signal_h,
+    // Enabled bitmask input
     input [7:0] enabled,
     // Outputs
     output [10:0] signal_mixed
 );
 
+    // TODO: Is there a more efficient way of doing this using logic instead of multiplication?
     wire [13:0] signal_mixed_prescale =
         enabled[0] * signal_a + 
         enabled[1] * signal_b + 
@@ -39,6 +47,7 @@ module mixer(
         enabled[6] * signal_g + 
         enabled[7] * signal_h;
 
+    // Normalize the mixed signal back to 11 bits
     assign signal_mixed = (signal_mixed_prescale >> 3);
 
 endmodule
