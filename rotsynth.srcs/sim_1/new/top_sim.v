ed0881041dfbc47ba54e7ef46645b03e0024214f
SpectrumX <wylieza@gmail.com>
2020-06-09 14:21:18 +0200

    simulation of the top module to try fix bugs, work in progress...

---
+++ rotsynth.srcs/sim_1/new/top_sim.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2020 11:10:17
+// Design Name: 
+// Module Name: top_sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_sim(
+
+    );
+endmodule
