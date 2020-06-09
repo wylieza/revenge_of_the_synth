8856559a9b5bf92c348c92668bc6d2f33378c39d
SpectrumX <wylieza@gmail.com>
2020-06-09 09:50:22 +0200

    selected preripherals and included them into top module

--- rotsynth.srcs/sources_1/new/top.v
+++ rotsynth.srcs/sources_1/new/top.v
@@ -1,26 +1,48 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.05.2020 09:49:18
-// Design Name: 
-// Module Name: top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************
+------ Module Description -----
+Author > Justin Wylie
+
+Description > This is the main driver module
+
+Usage:
+Accepts > All peripherals being used for input
+Returns > All peripherals being used for output
+
+Clocking & Reset:
+Requires Clock > Yes
+Reset Bit > No
+Enable Bit > No
+
+**************************************************************************************///////////////////////////////////////////////////////////////////////////////////
 
 
 module top(
-
+    input  CLK100MHZ,
+    input [15:0] SW,
+    input BTNC,
+    input BTNU,
+    input BTNL,
+    input BTNR,
+    input BTND,
+    output AUD_PWM, 
+    output AUD_SD,
+    output [15:0] LED,
+    output reg CA,
+	output reg CB,
+	output reg CC,
+	output reg CD,
+	output reg CE,
+	output reg CF,
+	output reg CG,
+	output reg DP,
+	output reg [7:0] AN = 8'hFF
     );
+    
+    
+    
+    
+    
+    
+    
 endmodule
